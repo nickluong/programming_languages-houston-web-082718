4e74e7c778114a79a3a886ce50e08c690c189c14
Nicholas  Luong <nckluong@gmail.com>
2018-08-27 03:36:55 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,6 +1,16 @@
 def reformat_languages(languages)
-  # your code here
-  new_hash => {}
-  
-  languages.each do
+  result = {
+  }
+
+  languages.each do |style, langs|
+  	langs.each do |lang, type|
+  		if result.include? |lang|
+  			result[lang][:style] << style 
+  		else
+	  		type[:style] = [style]
+	  		result[lang] = type
+	  	end
+  	end
+  end
+  result
 end
