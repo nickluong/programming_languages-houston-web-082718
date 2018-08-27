0d5ab7beec28359138eb1da8993ba553f0e236a0
Nicholas  Luong <nckluong@gmail.com>
2018-08-27 03:37:27 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
 def reformat_languages(languages)
-  # your code here
+  result = {
+  }
+
+  languages.each do |style, langs|
+  	langs.each do |lang, type|
+  		if result.include? lang
+  			result[lang][:style] << style 
+  		else
+	  		type[:style] = [style]
+	  		result[lang] = type
+	  	end
+  	end
+  end
+  result
 end
